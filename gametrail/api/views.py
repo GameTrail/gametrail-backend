b1cec8725dc9bf066e9f1ef374a9872bd76d5b98
Juan Carlos GR <juacargomram@gmail.com>
2023-03-06 15:25:20 +0100

    Added Search game by name

--- gametrail/api/views.py
+++ gametrail/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from gametrail.models import Game
 from gametrail.api.serializers import GameSerializer
+from rest_framework import filters
 
 class GameApiViewSet(ModelViewSet):
-    serializer_class = GameSerializer
     queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']

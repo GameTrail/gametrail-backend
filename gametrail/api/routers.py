93cd2d2700edc0f5ff0cf77014644f17442abc20
Julian <julonapar1@alum.us.es>
2023-03-08 20:33:18 +0100

    feat: terminada tarea BCK-07

--- gametrail/api/routers.py
+++ gametrail/api/routers.py
@@ -1,5 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from gametrail.api.views import GameApiViewSet
+from gametrail.api.views import GameApiViewSet, RatingApiViewSet, MinRatingTrailApiViewSet
 
 router_api = DefaultRouter()
 router_api.register(prefix='game', viewset=GameApiViewSet, basename='game')
+router_api.register(prefix='rating', viewset=RatingApiViewSet, basename='rating')
+router_api.register(prefix='minRating', viewset=MinRatingTrailApiViewSet, basename='minRating')
--- gametrail/api/views.py
+++ gametrail/api/views.py
@@ -1,7 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
-from gametrail.models import Game
-from gametrail.api.serializers import GameSerializer
+from gametrail.models import Game, Rating, MinRatingTrail
+from gametrail.api.serializers import GameSerializer, RatingSerializer, MinRatingTrailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class GameApiViewSet(ModelViewSet):
     serializer_class = GameSerializer
     queryset = Game.objects.all()
+
+class RatingApiViewSet(ModelViewSet):
+    serializer_class = RatingSerializer
+    queryset = Rating.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields  = ['ratedUser']
+
+
+class MinRatingTrailApiViewSet(ModelViewSet):
+    serializer_class = MinRatingTrailSerializer
+    queryset = MinRatingTrail.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields  = ['trail']

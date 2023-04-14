5259086ec0e2844e8ee4a007eaaf65ff8b0dd97a
jonatep <jonatep8@gmail.com>
2023-04-14 18:40:05 +0200

    chore: hechos tests de minRating

--- gametrail/api/tests.py
+++ gametrail/api/tests.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
-from gametrail.api.views import UserApiViewSet, POSTRatingAPIViewSet
-from gametrail.models import Rating, User
+from gametrail.api.views import UserApiViewSet, POSTRatingAPIViewSet, CreateMinRatingViewSet, GetMinRatingTrailApiViewSet, AddUserInTrailViewSet
+from gametrail.models import Rating, User, MinRatingTrail, Trail
 from rest_framework.test import APIRequestFactory
 from rest_framework import status
 from rest_framework.test import force_authenticate
-
-
 
 class RatingApiViewSetTestCase(TestCase):
 
@@ -99,3 +97,86 @@ class RatingApiViewSetTestCase(TestCase):
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data[0]['average_ratings']['AVAILABILITY'], 1)
+    
+
+class MinRatingApiViewSetTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.premiumUser = User.objects.create_user(username='miguelito', email='miguel_mola@gmail.com' ,avatar='kajnfawkef.jpg' ,password='miguelmolamucho')
+        self.premiumUser.plan = "Premium"
+        self.premiumUser.save()
+        self.standartUser = User.objects.create_user(username='pedrito', email='pedritodestroza@gmail.com' ,avatar='sfsfsfdsf.jpg' ,password='pedroteodia')
+
+        self.premiumUser_id = self.premiumUser.pk
+        self.standartUser_id = self.standartUser.pk
+
+        self.trail = Trail.objects.create(owner=self.premiumUser, 
+                                            name='Test Trail 1',
+                                            description='Test Trail 1',
+                                            startDate='2023-04-12',
+                                            maxPlayers='2', 
+                                            finishDate='2023-04-15')
+        
+        
+        self.min_rating = MinRatingTrail.objects.create(
+                                                        minRating = 2, 
+                                                        ratingType ='KINDNESS',
+                                                        trail = self.trail,
+                                                        user = self.premiumUser)
+        
+        self.url_rating = '/api/minRating/' 
+        self.url_create_rating = '/api/createMinRating'
+        self.url_add_user = '/api/addUserInTrail'
+
+    def test_get_ratings(self):
+    
+        request = self.factory.get(self.url_rating)
+        view = GetMinRatingTrailApiViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['ratingType'], 'KINDNESS')
+
+    def test_post_not_premium(self):
+       
+        data = {
+            'ratingType': 'KINDNESS',
+            'minRating': '1',
+            'trail': '1',
+            'user': '2'  
+        }
+
+        request = self.factory.post(self.url_create_rating, data, format='json')
+        force_authenticate(request, self.standartUser)
+        
+        view = CreateMinRatingViewSet.as_view()
+        response = view(request)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post(self):
+       
+        data = {
+            'ratingType': 'AVAILABILITY',
+            'minRating': '1',
+            'trail': '1',
+            'user': '1'  
+        }
+
+        request = self.factory.post(self.url_create_rating, data, format='json')
+        force_authenticate(request, self.premiumUser)
+        
+        view = CreateMinRatingViewSet.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data_add_user = {
+            'trail': '1',
+            'user': '2'  
+        }
+
+        request = self.factory.post(self.url_add_user, data_add_user, format='json')
+        force_authenticate(request, self.standartUser)
+        view = AddUserInTrailViewSet.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
